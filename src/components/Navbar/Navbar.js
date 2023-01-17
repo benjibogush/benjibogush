9e32830633f89aed995c5f6d4358352bca142287
Benji Bogush <70986668+benjibogush@users.noreply.github.com>
2023-01-17 03:42:50 +0000

    removed unused components and require fix for App directory

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { MenuItems } from "./MenuItems";
+import { Nav } from "react-bootstrap";
+
+export default function Navbar() {
+  return (
+    <Nav className="NavbarItems">
+      <h1 className="Navbar-logo">React</h1>
+      <div className="menu-icon"></div>
+          <ul>
+            {MenuItems.map((item, index) => {
+              return (
+                <li key={index}>
+                  <a className={item.cName} href={item.url}>
+                    {item.Title}
+                  </a>
+                </li>
+              );
+            })}
+          </ul>
+    </Nav>
+  );
+}
---
+++ views/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+import Navbar from "./src/components/Navbar/Navbar";
+// https://replit.com/@benjibogush/benjibogush#src/components/Navbar/Navbar.js
+// https://replit.com/@benjibogush/benjibogush#views/App.js
+
+export default function App() {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+}
